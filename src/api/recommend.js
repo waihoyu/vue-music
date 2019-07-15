98385a1190ddefc89984b45d659da625beeb5a8a
waihoyu <13701343809@qq.com>
2019-07-16 01:20:33 +0800

    修改错误

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -4,11 +4,12 @@
  * @date 2019/7/15 02:38
 */
 
-import  jsonp from 'jsonp'
+import  jsonp from 'common/js/jsonp'
 import {commonParams,options} from "./config";
 
 export  function  getRecommend() {
     const  url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+
     const data = Object.assign({}, commonParams, {
         platform: 'h5',
         uin: 0,
