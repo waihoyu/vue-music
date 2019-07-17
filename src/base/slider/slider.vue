702ac773e126cd044d78ec7a9e61b3f54e0973d7
waihoyu <13701343809@qq.com>
2019-07-17 20:28:04 +0800

    完成轮播图组件开发

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -11,7 +11,7 @@
             </slot>
         </div>
         <div class="dots">
-            <span class="dot" :class="{active: currentPageIndex === index}" v-for="(item, index) in dots">
+            <span class="dot"  v-for="(item, index) in dots" :class="{active: currentPageIndex === index}">
             </span>
         </div>
     </div>
@@ -39,10 +39,21 @@
 
         },
         mounted(){
-            setTimeout(()=>{
-                this._setSliderWidth()
+            setTimeout(() => {
+                this._setSliderWidth(false)
+                this._initDots()
                 this._initSlider()
+                if (this.autoPlay){
+                    this._play()
+                }
             },20)
+
+            window.addEventListener('resize',()=>{
+            if (!this.slider){
+                  return
+                }
+                this._setSliderWidth(true)
+            })
         },
         data() {
             return {
@@ -51,7 +62,7 @@
             }
         },
         methods: {
-            _setSliderWidth(){
+            _setSliderWidth(isResize){
                 this.children = this.$refs.sliderGroup.children
                 let width = 0
                 let sliderWidth = this.$refs.slider.clientWidth
@@ -62,25 +73,49 @@
                     child.style.width = sliderWidth + 'px'
                     width += sliderWidth
                 }
-                if (this.loop){
+                if (this.loop && !isResize){
                     width += 2 * sliderWidth
                 }
                 this.$refs.sliderGroup.style.width = width + 'px'
             },
+            _initDots(){
+                this.dots = new Array(this.children.length)
 
+            },
             _initSlider(){
                 this.slider = new BScroll(this.$refs.slider, {
                     scrollX: true,
                     scrollY: false,
                     momentum: false,
                     snap: true,
-                    snapLoop: true,
+                    snapLoop: this.loop,
                     snapThreshold: 0.3,
                     snapSpeed: 400,
                     // click: true
                 })
+                this.slider.on('scrollEnd', ()=>{
+                    let pageIndex = this.slider.getCurrentPage().pageX
+                     if (this.loop){
+                        pageIndex -= 1
+                     }
+                     this.currentPageIndex = pageIndex
+                     if (this.autoPlay){
+                        clearInterval(this.timer)
+                        this._play()
+                     }
+                })
 
+            },
+            _play(){
+                let pageIndex = this.currentPageIndex + 1
+                if (this.loop) {
+                    pageIndex += 1
+                }
+                 this.timer = setInterval(() => {
+                     this.slider.goToPage(pageIndex, 0, 400)
+                 },this.interval)
             }
+
         },
         computed: {
 
@@ -93,13 +128,13 @@
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
     @import "~common/stylus/variable.styl"
+
     .slider
-        min-height 1px
+        min-height 10px
         .slider-group
             position: relative
             overflow: hidden
             white-space: nowrap
-            background: red
             .slider-item
                 float: left
                 box-sizing border-box
@@ -110,27 +145,26 @@
                     width: 100%
                     overflow: hidden
                     text-decoration: none
-
                 img
                     display: block
                     width 100%
-
-            .dots
-                position: absolute
-                right: 0
-                left: 0
-                bottom: 12px
-                text-align: center
-                font-size: 0
-                .dot
-                    display: inline-block
-                    margin: 0 4px
-                    width: 8px
-                    height: 8px
-                    border-radius 50%
-                    background: $color-text-l
-                    &.active
-                        width: 20px
-                        border-radius 5px
-                        background: $color-text-ll
+        .dots
+            position: absolute
+            right: 0
+            left: 0
+            bottom: 12px
+            text-align: center
+            font-size: 0px
+            .dot
+                display: inline-block
+                margin: 0 4px
+                width: 8px
+                height: 8px
+                border-radius 50%
+                background: $color-text-l
+                &.active
+                    width: 20px
+                    border-radius 5px
+                    background: $color-text-ll
 </style>
+
